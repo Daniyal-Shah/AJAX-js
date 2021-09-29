a158b1a1cce6d14b24047b953e1ec4cd1b864b07
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-09-30 03:33:28 +0500

    fetch api

---
+++ 4-Fetch_Api.js
@@ -0,0 +1,11 @@
+fetch('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/2487956/')
+.then(result=>{
+//   console.log(result);  
+  return result.json();
+})
+.then(data=>{
+    console.log(data);
+})
+.catch(error=>{
+    console.log(error);  
+  });
