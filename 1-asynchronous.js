4a6ffcd8d073dd0b5c6725b70711073cda9d0c03
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-09-29 18:29:48 +0500

    callback hell

--- 1-asynchronous.js
+++ 1-asynchronous.js
@@ -1,1 +1,21 @@
-console.log("hello");
+function getRecipe()
+{
+    setTimeout(()=>{
+        const recipeID=[222,124,454,321];
+        console.log(recipeID);
+
+        setTimeout((id)=>
+        {
+            const recipe={title:"biryani", chief:"daniyal"}
+            console.log( `${id} : ${recipe.title}` )
+
+            setTimeout(chief=>{
+                const recipe2={title:"karahi", chief:"daniyal"}
+                console.log( recipe2 );
+            },1000, recipe.chief )
+
+        },1000, recipeID[2]);
+
+    },1500)
+}
+getRecipe();
